1001f4f8b279fc769b04c350f8360d2e6ed4f339
Thiago Nascimento <thiagonascim@yahoo.com>
2020-07-03 02:14:43 -0300

    adicionando jsonfy e jsons do simulador

---
+++ jsonfy.py
@@ -0,0 +1,46 @@
+import requests
+import urllib
+import json
+datag = {
+    "data": []
+}
+token = {"Authorization": "token 87420e5b10a3ec86bf18d1443e1590808a7364bc"}
+def WalkTree(data):
+    for item in data:
+        if item["type"] == "file":
+            path_parsed = urllib.parse.urlencode({"data":item["path"]})[5:]
+            obj = {
+                "name" : item["name"],
+                "path":item["path"]
+                "type" : "file",
+                "download" : item["download_url"],
+                "size" : item["size"],
+                "last_updated_at" : requests.get("https://api.github.com/repos/Txiag/sBotics/commits?path="+path_parsed, headers=token).json()[0]["commit"]["author"]["date"]
+            }
+            datag["data"].append(obj)
+        elif item["type"] == "dir":
+            req2 = requests.get
+            req2 = requests.get(item["_links"]["self"], headers=token).json()
+            WalkTree(req2)
+order = ["Linux AMD64.json", "Linux i386.json", "W32.json", "W64.json", "mac.json"]
+
+
+
+req = requests.get("https://api.github.com/repos/Txiag/sBotics/contents/", headers=token)
+req = req.json()
+cont = 0
+for item in req:
+    if (item["name"] == "teste.py"):
+        break
+    req2 = requests.get(item["_links"]["self"]).json()
+    print('-----Iniciando Busca do '+order[cont]+"-----")
+    WalkTree(req2)
+    print('-----Iniciando escrita do '+order[cont]+"-----")
+    jsonr = json.dumps(datag)
+    jsonx = open(order[cont], 'a')
+    jsonx.write(jsonr)
+    jsonx.close()
+    datag = {
+    "data": []
+    }
+    cont += 1
