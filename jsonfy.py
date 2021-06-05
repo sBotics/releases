e635af42997b8deaf2c294aad401843596821eb6
Julio Cesar <60150667+jvneto@users.noreply.github.com>
2021-06-04 23:17:01 -0300

    Update Last Version sBotics

--- jsonfy.py
+++ jsonfy.py
@@ -2,19 +2,24 @@ import requests
 import urllib
 import json
 datag = {
-        "version" : "1",
-        "download_link":"http://weduc.natalnet.br/sbotics",
+    "version": "1",
+    "download_link": "http://weduc.natalnet.br/sbotics",
     "data": []
 }
-token = {"Authorization": "token b073b687a287347b51eead6f0b4fee4a6dfc8695"}
+token = {"Authorization": "token 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"}
+
+
 def WalkTree(data):
     for item in data:
         try:
+            print(item["name"])
             if item["type"] == "file":
-                path_parsed = urllib.parse.urlencode({"data":item["path"]})[5:]
+                path_parsed = urllib.parse.urlencode(
+                    {"data": item["path"]})[5:]
                 path_normalized = item["path"]
                 path_normalized = path_normalized[path_normalized.find('/')+1:]
-                path_normalized = path_normalized[0:len(path_normalized)-len(item["name"])]
+                path_normalized = path_normalized[0:len(
+                    path_normalized)-len(item["name"])]
                 if item["name"][::-1][:4][::-1] == ".zip":
                     fformat = "zip"
                     print("entrou")
@@ -22,18 +27,19 @@ def WalkTree(data):
                 else:
                     fformat = "simple"
                 obj = {
-                    "name" : item["name"],
-                    "path":path_normalized,
-                    "type" : "file",
-                    "download" : item["download_url"],
-                    "size" : item["size"],
-                    "last_updated_at" : requests.get("https://api.github.com/repos/Txiag/sBotics/commits?path="+path_parsed, headers=token).json()[0]["commit"]["author"]["date"],
-                    "format" : fformat
+                    "name": item["name"],
+                    "path": path_normalized,
+                    "type": "file",
+                    "download": item["download_url"],
+                    "size": item["size"],
+                    "last_updated_at": requests.get("https://api.github.com/repos/Txiag/sBotics/commits?path="+path_parsed, headers=token).json()[0]["commit"]["author"]["date"],
+                    "format": fformat
                 }
                 datag["data"].append(obj)
             elif item["type"] == "dir":
                 req2 = requests.get
-                req2 = requests.get(item["_links"]["self"], headers=token).json()
+                req2 = requests.get(
+                    item["_links"]["self"], headers=token).json()
                 WalkTree(req2)
         except:
             jsonr = json.dumps(data)
@@ -41,19 +47,23 @@ def WalkTree(data):
             jsonx.write(jsonr)
             jsonx.close()
             exit()
-order = ["Linux AMD64.json", "W32.json", "W64.json", "mac.json"]
-names = ["Linux AMD64", "W32", "W64", "mac"]
 
-req = requests.get("https://api.github.com/repos/Txiag/sBotics/contents/", headers=token)
+
+# order = ["Linux AMD64.json", "W32.json", "W64.json", "mac.json"]
+# names = ["Linux AMD64", "W32", "W64", "mac"]
+order = ["mac.json"]
+names = ["mac"]
+req = requests.get(
+    "https://api.github.com/repos/Txiag/sBotics/contents/", headers=token)
 req = req.json()
 cont = 0
-print(req);
+print(req)
 for item in req:
     if cont == 5:
         break
     if item["name"] in names:
-        print("Entrou no "+ item["name"])
-        req2 = requests.get(item["_links"]["self"]).json()
+        print("Entrou no " + item["name"])
+        req2 = requests.get(item["_links"]["self"], headers=token).json()
         print('-----Iniciando Busca do '+order[cont]+"-----")
         WalkTree(req2)
         print('-----Iniciando escrita do '+order[cont]+"-----")
@@ -62,9 +72,9 @@ for item in req:
         jsonx.write(jsonr)
         jsonx.close()
         datag = {
-        "version" : "1",
-        "download_link":"http://weduc.natalnet.br/sbotics",
-        "data": [],
-        
+            "version": "1",
+            "download_link": "http://weduc.natalnet.br/sbotics",
+            "data": [],
+
         }
         cont += 1
